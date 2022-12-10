367fcb8ae4c1df4865dc1ee88fb42af99b319044
Igor <211353@mail.ru>
2022-12-10 18:46:55 +0300

    Выполнил первое ДЗ

---
+++ src/lesson1/homework1.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class homework1 {
+    public static void main(String[] args) {
+    }
+        private static void task1(){
+            System.out.println("Hy");
+        }
+    }
+
